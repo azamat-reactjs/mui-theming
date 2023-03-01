fc8515f31d54b33ffdb3a4eb40111f291ccf8bfd
azamatreactjs <azamatreactjs@gmail.com>
2023-03-01 16:57:40 +0500

    Remove import App and import index.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import CssBaseline from '@mui/material/CssBaseline'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Root } from './routes/Root'
