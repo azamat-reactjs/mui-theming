190e9aa82e94f5817295ca05ed6e1ac5460df6bd
azamatreactjs <azamatreactjs@gmail.com>
2023-03-01 16:57:14 +0500

    Add react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+
 import CssBaseline from '@mui/material/CssBaseline'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Root } from './routes/Root'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <CssBaseline />
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
