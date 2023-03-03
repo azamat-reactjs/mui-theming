121609b2a0f1fcccacf5d4b49d1c3a17aa4a2986
azamatreactjs <azamatreactjs@gmail.com>
2023-03-03 18:57:50 +0500

    Test font weight for prod

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,15 @@ import { Typography } from '@mui/material'
 export const Footer = () => {
   return (
     <footer className="footer">
-      <Typography fontWeight={700}>FOOTER</Typography>
+      <Typography fontWeight={100}>Typography 100</Typography>
+      <Typography fontWeight={200}>Typography 200</Typography>
+      <Typography fontWeight={300}>Typography 300</Typography>
+      <Typography fontWeight={400}>Typography 400</Typography>
+      <Typography fontWeight={500}>Typography 500</Typography>
+      <Typography fontWeight={600}>Typography 600</Typography>
+      <Typography fontWeight={700}>Typography 700</Typography>
+      <Typography fontWeight={800}>Typography 800</Typography>
+      <Typography fontWeight={900}>Typography 900</Typography>
     </footer>
   )
 }
