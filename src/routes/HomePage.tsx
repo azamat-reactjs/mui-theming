8926f4513d493bc0512f7c690d53f9243f2060ec
azamatreactjs <azamatreactjs@gmail.com>
2023-03-03 19:04:09 +0500

    Test font weight for prod

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -1,7 +1,19 @@
+import { Typography } from '@mui/material'
+
 export const HomePage = () => {
   return (
     <section className="home-page" id="home-page">
-      <div>HOME PAGE</div>
+      <div>
+        <Typography fontWeight={100}>Thin 100</Typography>
+        <Typography fontWeight={200}>ExtraLight 200</Typography>
+        <Typography fontWeight={300}>Light 300</Typography>
+        <Typography fontWeight={400}>Regular 400</Typography>
+        <Typography fontWeight={500}>Medium 500</Typography>
+        <Typography fontWeight={600}>SemiBold 600</Typography>
+        <Typography fontWeight={700}>Bold 700</Typography>
+        <Typography fontWeight={800}>ExtraBold 800</Typography>
+        <Typography fontWeight={900}>Black 900</Typography>
+      </div>
     </section>
   )
 }
